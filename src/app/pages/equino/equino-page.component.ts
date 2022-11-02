9c46d5522de39030a583b93a52bdab4d402dcda0
Pedro Lenonn <pedro21900@hotmail.com>
2022-11-02 13:06:06 -0300

    feat: equino-page

---
+++ src/app/pages/equino/equino-page.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-catalog',
+    templateUrl: './equino-page.component.html',
+    styleUrls: ['./equino-page.component.scss'],
+})
+export class EquinoPage implements OnInit {
+
+
+    constructor() {
+    }
+
+     ngOnInit() {
+
+    }
+
+}
