658337330d8e32225f4c8c7351d1a835f4e3c063
Pedro Lenonn <pedro21900@hotmail.com>
2022-11-10 09:17:18 -0300

    refactor: regra de negócio

--- src/app/pages/login/login-validation/login-validation.page..ts
+++ src/app/pages/login/login-validation/login-validation.page..ts
@@ -52,12 +52,13 @@ export class LoginValidationPage implements OnInit {
             const typeProfile: GetResult = await Preferences.get({key: 'typeProfile'});
             const cpfOrCnpj: string = this.loginForm.value.cpfOrCnpj.replace(/[^a-zA-Z0-9 ]/g, '');
             const loading = await this.loadingCtrl.create({ message: 'Validando....' });
+            loading.present();
             const urlBase:string=this.loginForm.value.estado[environment.columnTbEstado];
             await Preferences.set({key: 'estadoApiResource', value: urlBase});
             await this.sqLiteService.downloadDatabase(false, `${urlBase}rest/passaporteEquestre/criacaoTabelasSqlite`);
             await this.sqLiteService.downloadDatabase(true, `${urlBase}rest/passaporteEquestre/sincronismoInicial`);
             await this.sqLiteService.downloadDatabase(true, '/assets/views-database.json');
-            loading.present();
+
             //this.showLoading();
             from(this.validatePfOrPj
                 .checkCnpjOrCpfByInTipoUsuario(cpfOrCnpj, typeProfile.value))
